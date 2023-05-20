f77aa8a42cfe0d6d282f2ec86f2653051d3fb3eb
Iliraa <61836150+Ilirr@users.noreply.github.com>
2023-05-20 21:26:59 +0200

    Added json-api.json, MSI.java, MyAdapter.java, MyViewHolder.java

---
+++ app/src/main/java/com/example/project/MyViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder
+{
+    public TextView MSI_Name;
+    public MyViewHolder(View itemView) {
+        super(itemView);
+        //mountainName = itemView.findViewById(R.id.mountain_name);
+    }
+}
