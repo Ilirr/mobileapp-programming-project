c5698781700b4c294d0d3249fae0dd64d0a6059d
Iliraa <61836150+Ilirr@users.noreply.github.com>
2023-05-20 21:38:58 +0200

    Implemented MSI.java and MyViewHolder.java classes. Added TextViews to activity.

--- app/src/main/java/com/example/project/MSI.java
+++ app/src/main/java/com/example/project/MSI.java
@@ -1,4 +1,24 @@
 package com.example.project;
 
 public class MSI {
+
+    public String ID;
+    public String Name;
+    public String Company;
+    public String Location;
+    public String Category;
+    public String Score; //auxdata
+    public int Cost;
+
+    public MSI (String ID, String Name, String Company, String Location, String Category, String Score, int Cost)
+    {
+        this.ID = ID;
+        this.Name = Name;
+        this.Company = Company;
+        this.Location = Location;
+        this.Category = Category;
+        this.Score = Score;
+        this.Cost = Cost;
+
+    }
 }
--- app/src/main/java/com/example/project/MyViewHolder.java
+++ app/src/main/java/com/example/project/MyViewHolder.java
@@ -7,9 +7,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MyViewHolder extends RecyclerView.ViewHolder
 {
-    public TextView MSI_Name;
+    public TextView ID;
+    public TextView name;
+    public TextView location;
+    public TextView category;
+    public TextView company;
+    public TextView cost;
+    public TextView score;
+
     public MyViewHolder(View itemView) {
         super(itemView);
-        //mountainName = itemView.findViewById(R.id.mountain_name);
+        ID = itemView.findViewById(R.id.ID);
+        name = itemView.findViewById(R.id.name);
+        location = itemView.findViewById(R.id.location);
+        category = itemView.findViewById(R.id.category);
+        company = itemView.findViewById(R.id.company);
+        cost = itemView.findViewById(R.id.cost);
+        score = itemView.findViewById(R.id.score);
     }
+
 }
