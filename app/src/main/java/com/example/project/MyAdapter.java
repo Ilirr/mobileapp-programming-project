82ae38c6f7dac7d04267563ceb02cb79aa85d63c
Iliraa <61836150+Ilirr@users.noreply.github.com>
2023-05-22 17:47:04 +0200

    Implemented method in MyViewHolder, AAdded msi_item.xml, Implemented method in MyAdapter. Added activity_about.xml, activity_detailed_view.xml

--- app/src/main/java/com/example/project/MyAdapter.java
+++ app/src/main/java/com/example/project/MyAdapter.java
@@ -1,20 +1,36 @@
 package com.example.project;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder>
 {
+
+
+    private ArrayList<MSI> MSI_List;
+
+    public MyAdapter(ArrayList<MSI> list)
+    {
+        MSI_List = list;
+    }
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+     View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msi_item, parent, false);
+     return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        MSI msi = MSI_List.get(position);
+        holder.set(msi);
 
     }
 
--- app/src/main/java/com/example/project/MyViewHolder.java
+++ app/src/main/java/com/example/project/MyViewHolder.java
@@ -26,4 +26,17 @@ public class MyViewHolder extends RecyclerView.ViewHolder
         score = itemView.findViewById(R.id.score);
     }
 
+    public void set(MSI msi)
+    {
+        ID.setText(msi.ID);
+        name.setText(msi.Name);
+        company.setText(msi.Company);
+        location.setText(msi.Location);
+        category.setText(msi.Category);
+        cost.setText(String.valueOf(msi.Cost));
+        score.setText(msi.Score);
+       // sizeTextView.setText(String.valueOf(msi.getSize()));
+
+    }
+
 }
